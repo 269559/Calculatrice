0b21ae42c40b583465d62e31ecb1a4264256c229
FEKIH saber <saberfekih44@gmail.com>
2024-02-28 13:14:32 +0100

    Calculatrice

---
+++ apps.js
@@ -0,0 +1,30 @@
+const info = document.querySelector('.infos')
+
+const altBtns = document.querySelectorAll('input[type="button"]')
+
+function val(resultat){
+    form.fenetre.value = form.fenetre.value + resultat
+
+
+}
+
+function calcule(){
+    if(form.fenetre.value == ""){
+        info.innerText = 'Veuillez ecrire un chiffre';
+        allBtns.forEach(btn => {
+            btn.addEventListener('click', function(){
+                info.innerText = ""
+
+            })
+       
+})
+    }else {
+        form.fenetre.value= eval(form.fenetre.value)
+    }
+}
+
+function suppr(){
+
+    form.fenetre.value='';
+
+}
